6f2d412a0f44ecac7c25392dceb1e19af701b5b7
IRENE\Kian <a.nadi@hafiz-co.com>
2021-03-10 12:55:41 +0330

    first commit

---
+++ src/main/java/com/sample/nasa/NasasampleApplication.java
@@ -0,0 +1,13 @@
+package com.sample.nasa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NasasampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NasasampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sample/nasa/NasasampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sample.nasa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasasampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
